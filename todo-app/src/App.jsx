4144a86f19554fd3361a8a4627f1fc9418e34fe0
Aung Chan Myae <aungchanmyae10997@gmail.com>
2024-11-25 22:41:36 +0700

    Part 2

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,21 +2,37 @@ import { Header } from "./components/Header";
 import { Tabs } from "./components/Tabs";
 import { TodoList } from "./components/TodoList";
 import { Todoinput } from "./components/TodoInput";
+import { useState } from "react";
 
 function App() {
-  const todos = [
-    { input: "Hello! Add your first todo!", complete: true },
-    { input: "Get the gecories", complete: false },
-    { input: "Learn how to web design", complete: false },
-    { input: "Say hi to gran gran", complete: true },
-  ];
+  // const todos = [
+  //   { input: "Hello! Add your first todo!", complete: true },
+  //   { input: "Get the gecories", complete: false },
+  //   { input: "Learn how to web design", complete: false },
+  //   { input: "Say hi to gran gran", complete: true },
+  // ];
+
+  const [todos, setTodos] = useState([{ input: "Hello! Add your first todo!", complete: true }])
+
+  function handleAddTodo(newTodo){
+    const newTodoList = [...todos, {input: newTodo, complete: false}]
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(){
+
+  }
+
+  function handleDeleteTodo(){
+
+  }
 
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
+      <Todoinput  handleAddTodo={ handleAddTodo }/>
       <TodoList todos={todos} />
-      <Todoinput />
     </>
   );
 }
--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -3,12 +3,22 @@ import { TodoCard } from "./TodoCard";
 export function TodoList(props) {
   const { todos } = props;
 
+  
   const tab = "All";
-
+  
+  const filterTodoList =  tab ==='ALl' ? 
+  todos: []
+  tab === "Completed" ? 
+  todos.filter(val => val.complete):
+  todos.filter(val => !val.complete) 
+ 
   return (
     <>
-      {todos.map((todo, todoIndex) => {
-        return <TodoCard key={todoIndex} todosIndex={todoIndex} {...props} />;
+      {filterTodoList.map((todo, todoIndex) => {
+        return <TodoCard 
+        key={todoIndex} 
+        todo = {todo} />;
+        
       })}
     </>
   );
